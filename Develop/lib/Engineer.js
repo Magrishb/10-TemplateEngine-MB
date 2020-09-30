da2962418f23c3cff70fdbaf37b9d079b6213290
Magrishb <bmagrish@gmail.com>
2020-09-30 18:49:19 -0500

    Added code into Intern.js: created intern class and add school along with the parent peramiters.

--- Develop/lib/Engineer.js
+++
@@ -1,7 +0,0 @@
-// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
-class Engineer {
-    constructor(){
-
-    }
-}
-mondule.exports = Engineer;
