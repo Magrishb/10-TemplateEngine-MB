1fd153e9d019e080bf4a46132e88397572bf20f7
Magrishb <bmagrish@gmail.com>
2020-09-30 21:13:21 -0500

    app.js: Work on question array

--- app.js
+++ app.js
@@ -13,6 +13,25 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+const questionArray = [
+    {
+        type: "list",
+        name: "role",
+        message: "What is your role",
+        choices: ["Manger", "Engineer", "Intern"],
+    },
+    {
+        type: "input",
+        name: "name",
+        message: "What is your name?",
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is your employee id?",
+    },
+];
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
